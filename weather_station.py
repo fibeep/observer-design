dee062181e1c0b8cb622c3736197333513315bb3
Salo <salomonco@gmail.com>
2022-10-26 11:40:45 -0500

    statistics display class implemented

--- weather_station.py
+++ weather_station.py
@@ -94,6 +94,63 @@ class CurrentConditionsDisplay(Observer):
 # TODO: implement StatisticsDisplay class and ForecastDisplay class.
 
 
+    class StatisticsDisplay(Observer):
+        def __init__(self, weatherData):
+            super().__init__(weatherData)
+
+            # Temperature Data
+            self.min_temp = float('inf')
+            self.average_temp = 0
+            self.max_temp = float('-inf')
+
+
+            # Humidity Data
+            self.min_humidity = float('inf')
+            self.average_humidity = 0
+            self.max_humidity = float('-inf')
+
+            # Pressure Data
+            self.min_pressure = float('inf')
+            self.average_pressure = 0
+            self.max_pressure = float('-inf')
+            
+        def calculate_stats(self):
+            
+            # Claculations for Temperature Data
+            self.min_temp = round(min(self.min_temp, self.temp), 2)
+            self.max_temp = round(max(self.max_temp, self.temp), 2)
+            self.average_temp = round((self.max_temp + self.min_temp) / 2, 2)
+            
+            # Claculations for Humidity Data
+            self.min_humidity = round(min(self.min_humidity, self.humidity), 2)
+            self.max_humidity = round(max(self.max_humidity, self.humidity), 2)
+            self.average_humidity = round(
+                (self.max_humidity + self.min_humidity) / 2, 2)
+            
+            # Claculations for Pressure Data
+            self.min_pressure = round(min(self.min_pressure, self.pressure), 2)
+            self.max_pressure = round(max(self.max_pressure, self.pressure), 2)
+            self.average_pressure = round(
+                (self.max_pressure + self.min_pressure) / 2, 2)
+            
+        def display_data(self):
+            
+            self.calculate_stats()
+            
+            print("Weather Statistics Today: ")
+            print('-------------------------')
+            print("Minimum | Average | Maximum Temperature ")
+            print(self.min_temp, " | ", self.average_temp, ' | ', self.max_temp)
+            print('-------------------------')
+            print("Minimum | Average | Maximum Humidity ")
+            print(self.min_humidity, " | ", self.average_humidity, ' | ', self.max_humidity)
+            print('-------------------------')
+            print("Minimum | Average | Maximum Pressure ")
+            print(self.min_pressure, " | ", self.average_pressure, ' | ', self.max_pressure)
+
+
+
+
 class WeatherStation:
     def main(self):
         weather_data = WeatherData()
